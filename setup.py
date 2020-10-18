061141184f07aac9e3cf2d7517bd4909b20b22e4
wassname <wassname@users.noreply.github.com>
2020-10-18 08:42:23 +0800

    mv

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='seq2seq-time',
+    name='seq2seq_time',
     packages=find_packages(),
     version='0.1.0',
     description='Using sequence to sequence interfaces for timeseries regression',
